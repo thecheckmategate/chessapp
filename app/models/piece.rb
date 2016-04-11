a1dba7dfc2ec0e4e7e96b1f7cd33f2ec8b65e065
Dong-Hyun Kim <dhexonian@aol.com>
2016-04-11 02:22:14 +0000

    Add executable_move? method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,7 +3,12 @@ class Piece < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :game
 
-	def move_to!(new_x, new_y)
+	def executable_move?(x_target, y_target)
+		return false unless valid_move?(x_target, y_target) # => if the move isn't valid for its type 
+		return false unless on_board?(x_target, y_target) # => if the piece tries to move outside the board 
+		return false if obstacle_between?(x_target, y_target) # => if there is obstruction in between 
+		return false if no_move?(x_target, y_target) # => if the piece doesn't move at all 
+	end 
 
 		# This move_to method should handle the following cases:
 	# Check to see if there is a piece in the location it’s moving to.
