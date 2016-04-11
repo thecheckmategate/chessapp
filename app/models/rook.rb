98b02f6ea7e7a3e97831bf5b222e79ee2d919ad9
Dong-Hyun Kim <dhexonian@aol.com>
2016-04-11 02:18:17 +0000

    Add valid move for rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,2 +1,8 @@
 class Rook < Piece
+
+	def valid_move?(x_target, y_target)
+ 		(x_position == x_target) || (y_position == y_target)
+ 		# allows for move to any number of squares vertically or horizontally 
+	end 
+
 end
