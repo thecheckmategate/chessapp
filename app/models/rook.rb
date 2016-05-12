beab553ce4919a4eb743172819421e9cfe6ea15b
Dong-Hyun Kim <dhexonian@aol.com>
2016-05-12 21:35:07 +0000

    fix executable move

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -5,7 +5,7 @@ class Rook < Piece
   end
 
 	def valid_move?(x_target, y_target)
- 		(x_position == x_target) || (y_position == y_target)
+ 		((x_position == x_target) || (y_position == y_target))
  		# allows for move to any number of squares vertically or horizontally 
 	end 
 
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Rook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "does a valid move" do 
+  	Rook.create(x_position: 2, y_position: 2)
+  	rook = Rook.last
+  	expect(rook.executable_move?(2,4)).to be_truthy
+  end
+
 end
