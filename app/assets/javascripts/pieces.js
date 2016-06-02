be133d4f9d6190a8863c72d38f7070d743442487
Mike Hoitomt <mhoitomt@lgscout.com>
2016-06-02 06:48:08 -0500

    Update the draggable logic to gather piece information from the UI before sending to the server

--- app/assets/javascripts/pieces.js
+++ app/assets/javascripts/pieces.js
@@ -1,82 +1,33 @@
 $(function() {
-	$('.piece').draggable({ containment: "table", revert: 'invalid' });
+  $('.piece').draggable({ containment: "table", revert: 'invalid' });
 
-	$('td').droppable({
-		drop: function(ev, ui) {
-			$( this ).addClass( "ui-state-highlight" )
+  $('td').droppable({
+    drop: function(ev, ui) {
+      $( this ).addClass( "ui-state-highlight" )
+
+      // Get Dropped item and drop location
       var dropped = ui.draggable;
       var droppedOn = $(this);
+
+      // Move the Piece in the UI
       $(droppedOn).droppable("disable");
       $(dropped).parent().droppable("enable");
       $(dropped).detach().css({top: 0, left: 0}).appendTo(droppedOn);
+
+      // Send the new coordinates to the controller
       var data = JSON.parse(dropped.attr("data"));
+      var url = "/games/" + data.game_id + "/pieces/" + data.id;
+      data.x_position = 0 // Add x position here, need to obtain from board
+      data.y_position = 0 // Add y position here, need to obtain from board
       $.ajax({
-        url: '/games/'+ data.game_id + '/pieces/' + data.id,
+        url: url,
         type: 'PUT',
+        data: data,
         success: function(result) {
-            // Do something with the result
+          console.log(result);
         }
       });
-  	}
-	});
+    }
+  });
 
 });
-
-
-// $(function() {
-// 	$('.piece').draggable({ containment: "table", revert: 'invalid' });
-
-// 	$('td').droppable({
-//     drop: function(ev, ui) {
-//       var dropped = ui.draggable;
-//       var droppedOn = $(this);
-//       $(droppedOn).droppable("disable");
-//       $(dropped).parent().droppable("enable");
-//       $(dropped).detach().css({top: 0, left: 0}).appendTo(droppedOn);
-//   	}
-// 	});
-
-// 	$('td').not('td:empty').droppable("disable");
-
-// });
-
-
-
-// $(function() {
-//     $( ".piece" ).draggable({
-//     	containment: "table",
-//     	revert: 'invalid'
-//     });
-//     $( "td" ).droppable({
-//       accept: ".piece",
-//       activeClass: "ui-state-hover",
-//       hoverClass: "ui-state-active",
-//       drop: function( event, ui ) {
-        // $( this )
-        //   .addClass( "ui-state-highlight" )
-//       }
-//     });
-//   });
-
-
-
-
-
-
-
-// $(function() {
-// 	$(".piece").draggable();
-// 	$(".piece").droppable({
-// 		drop: function() {
-// 			alert("piece moved");
-// 		}
-// 	});
-// });
-
-
-
-
-// $(function(){
-// 	console.log("login");
-// 	$(".piece").draggable();
-// });
