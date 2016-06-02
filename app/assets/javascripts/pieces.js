7979348db3987c53076b8053e4d4f09ad071149d
Nick Martinez <23nicholasmartinez@gmail.com>
2016-06-02 00:30:11 +0000

    Refactored pieces.js

--- app/assets/javascripts/pieces.js
+++ app/assets/javascripts/pieces.js
@@ -2,16 +2,15 @@ $(function() {
 	$('.piece').draggable({ containment: "table", revert: 'invalid' });
 
 	$('td').droppable({
-		accept: ".piece",
-    activeClass: "ui-state-hover",
-    hoverClass: "ui-state-active",
-    drop: function(ev, ui) {
-    	$( this )
-          .addClass( "ui-state-highlight" )   
+		drop: function(ev, ui) {
+			$( this ).addClass( "ui-state-highlight" )
+      var dropped = ui.draggable;
+      var droppedOn = $(this);
+      $(droppedOn).droppable("disable");
+      $(dropped).parent().droppable("enable");
+      $(dropped).detach().css({top: 0, left: 0}).appendTo(droppedOn);   
   	}
 	});   
-
-	$('td').not('td:empty').droppable("disable");
 
 });
 
